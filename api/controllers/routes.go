a8cb7921efcb750949006007faec59c1dfbc8f6c
hasan <blackbee08@gmail.com>
2021-06-28 20:01:02 +0700

    Added server routes

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,10 +1,10 @@
 package controllers
 
 func (server *Server) initializeRoutes() {
-	//server.Router.HandleFunc("/guest_list", a.GetGuestLists).Methods("GET")
-	//server.Router.HandleFunc("/guest_list/{name}", a.CreateGuest).Methods("POST")
-	//server.Router.HandleFunc("/guests/{name}", a.UpdateGuest).Methods("PUT")
-	//server.Router.HandleFunc("/guests/{name}", a.DeleteGuest).Methods("DELETE")
-	//server.Router.HandleFunc("/guests", a.GetArrivedGuests).Methods("GET")
-	//server.Router.HandleFunc("/seats_empty", a.SeatsEmpty).Methods("GET")
+	server.Router.HandleFunc("/guest_list", server.GetGuestLists).Methods("GET")
+	server.Router.HandleFunc("/guest_list/{name}", server.CreateGuest).Methods("POST")
+	server.Router.HandleFunc("/guests/{name}", server.UpdateGuest).Methods("PUT")
+	server.Router.HandleFunc("/guests/{name}", server.DeleteGuest).Methods("DELETE")
+	server.Router.HandleFunc("/guests", server.GetArrivedGuests).Methods("GET")
+	server.Router.HandleFunc("/seats_empty", server.SeatsEmpty).Methods("GET")
 }
